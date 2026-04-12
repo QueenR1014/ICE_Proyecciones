5dd0bc342d51436bd189e3e60319048cdc8ad51c
QueenR1014 <juanreinareyes@hotmail.com>
2026-04-11 20:56:14 -0500

    Proyecciones Normales terminadas

--- proyección_PIB.R
+++ proyección_PIB.R
@@ -2,18 +2,19 @@ library(readxl)
 library(dplyr)
 # Inserción PIB por oferta
 
-PIB_oferta = read_excel("Cuadros_Prod_Gasto.xlsx", sheet = "PIB oferta anual")
-PIB_oferta = select(PIB_oferta, 2:28)
+PIB_oferta = read_excel("Cuadros_Prod_Gasto.xlsx", sheet = "PIB oferta anual NORMAL")
+PIB_oferta = select(PIB_oferta, 2:22)
 PIB_oferta = na.omit(PIB_oferta)
 # Convertir primera fila en nombres de columnas
 colnames(PIB_oferta) = PIB_oferta[1, ]
 PIB_oferta = PIB_oferta[-1, ]
 
-library(GGally)
 
-datos = as.data.frame(t(PIB_oferta[1:13, 2:22]))
-colnames(datos) = PIB_oferta[[1]][1:13]
-R = cor(datos)
-ggpairs(datos, style = "upper")
+datos = as.data.frame(t(PIB_oferta[1:15, 1:21]))
+colnames(datos) = PIB_oferta[[1]][1:15]
+
+
+model = lm(PIB ~ ., data = datos)
+summary(model)
 
 
